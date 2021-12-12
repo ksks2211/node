049fcf646ee1d3fd8f42e63ce72ca8ae981309fc
ksks2211 <44663376+ksks2211@users.noreply.github.com>
2021-12-12 19:50:07 +0900

    merge fs examples to fs directory

--- file_system_stream_buffer/write/writefile.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-
-fs.writeFile('../file/writefile.txt', 'write file', (err) => {
-  if (err) console.error(err)
-  else console.log('Completed')
-})
--- file_system_stream_buffer/write/writefilesync.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs')
-
-try {
-  fs.writeFileSync('../file/writefilesync.txt', 'write file sync')
-} catch (err) {
-  console.error(err)
-}
